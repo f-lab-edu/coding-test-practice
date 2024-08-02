de185033e14c0687f4e8434f085b1404f50e6971
woobuntu <woobuntu2022@gmail.com>
2024-08-02 15:05:08 +0900

    문자열에 들어 있는 모든 공백을 '%20'으로 바꿔주는 메서드 작성

---
+++ array-and-string/to-url-string.ts
@@ -0,0 +1,13 @@
+export const toUrlString = (input: string): string => {
+  const array = input.split("");
+
+  for (let i = 0; i < array.length; i++) {
+    const char = array[i];
+
+    if (char === " ") {
+      array[i] = "%20";
+    }
+  }
+
+  return array.join("");
+};
